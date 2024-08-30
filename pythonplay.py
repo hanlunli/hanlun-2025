69987b2902974472ef90d6eb3bbdcf979842a950
hanlunli <hanlunli.007@gmail.com>
2024-08-30 08:43:05 -0700

    monkey script works!

--- pythonplay.py
+++ pythonplay.py
@@ -7,18 +7,18 @@ monkey_article_words = [i for i in monkey.content.split()]
 
 monkeylist = ['ape', 'apes', 'monkey', 'monkeys', 'gorilla', 'orangutan', 'chimpanzee', 'baboon', 'gorillas', 'orangutans', 'chimpanzees', 'chimps', 'chimp', 'baboons']
 monkey_emojis = [
-    ":monkey_face:",  # 🐵
-    ":monkey:",       # 🐒
-    ":see_no_evil:",  # 🙈
-    ":hear_no_evil:", # 🙉
-    ":speak_no_evil:",# 🙊
-    ":gorilla:",      # 🦍
-    ":orangutan:"     # 🦧
+    ":monkey_face:",
+    ":monkey:",
+    ":see_no_evil:",
+    ":hear_no_evil:",
+    ":speak_no_evil:",
+    ":gorilla:",
+    ":orangutan:"
 ]
 # print(monkey.content, monkey.url)
 
 for i in monkey_article_words:
-    if i.lower() in monkeylist:
-        print(emojize(monkey_emojis[random.randint(0, len(monkey_emojis)-1)]), end = " ")
+    if i.lower() in monkeylist or i[:-1].lower() in monkeylist or i[1:-1].lower() in monkeylist or i[1:-2].lower() in monkeylist:
+        print(emojize(monkey_emojis[random.randint(0, len(monkey_emojis)-1)], language='alias'), end = " ")
     else:
         print(i, end = " ")
